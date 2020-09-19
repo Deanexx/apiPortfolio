54e93c413c564b602656396cfb23c644d81b1297
Eldar <tailov47@gmail.com>
2020-09-18 20:38:24 -0700

    update with AceessControll

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const app = express(); // function on calling will add a lot of actions
 const emailRouter = require('./routes/emailRouter');
 
 app.use(express.json()); // req.body could be usable
-
+app.use(function(req, res, next){
+    res.header("Access-Control-Allow-Origin", "localhost.TLD"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+})
 app.use('/api', emailRouter);
 
 module.exports = app;
